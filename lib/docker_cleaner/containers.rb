d11d37308ba261cc0517f7ded77fb58d25843c94
Soulou <leo@unbekandt.eu>
2015-04-08 12:57:17 +0200

    Remove crashed container older than a week

--- lib/docker_cleaner/containers.rb
+++ lib/docker_cleaner/containers.rb
@@ -2,8 +2,9 @@ module DockerCleaner
 class Containers
   def run
     # Remove stopped container which stopped with code '0'
+    one_week_ago = Time.now.to_i - 7 * 24 * 3600
     Docker::Container.all(all: true).select{ |container| 
-      container.info["Status"].include?("Exited (0)")
+      container.info["Status"].include?("Exited (0)") || container.info["Status"].include?("Exited (") && container.info["Created"].to_i < one_week_ago
     }.each do |container|
       $stdout.write "Remove #{container.id[0...10]} - #{container.info["Image"]} - #{container.info["Names"][0]}"
       container.remove
